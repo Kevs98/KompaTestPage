12226a5965b042bad7b69065fca1600cf453e8e5
kevinE98 <kevinpalma657@gmail.com>
2020-05-08 14:50:20 -0600

    Angular

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDxG2q7Sjc68ELyykVnNg4g-GmzmIGDTs8",
+    authDomain: "kompaform.firebaseapp.com",
+    databaseURL: "https://kompaform.firebaseio.com",
+    projectId: "kompaform",
+    storageBucket: "kompaform.appspot.com",
+    messagingSenderId: "197834565418",
+    appId: "1:197834565418:web:8fd10d1891e38056392053",
+    measurementId: "G-3787H5X8T8"
+  }
 };
